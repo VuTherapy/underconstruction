e3e1c493db45873918ea3a30669a69da8d03cf63
Russ Petersen <russ@petesoft.com>
2012-03-26 09:28:16 -0600

    try again

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Underconstruction::Application.config.secret_token = 'd6d2d0a77c646bb541b195364e5e811ec6f4e254a1715aebd069f4232f74b304788946226ed3013ddac4d838c06f4fa12e1eb75aa9074e95ad8db16d7748a571'
